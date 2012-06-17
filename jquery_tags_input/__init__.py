764124142db7b0bb4bd813560ac2807757d3953c
WoLpH <Rick@Fawo.nl>
2012-06-18 01:14:26 +0200

    pep8

--- jquery_tags_input/__init__.py
+++ jquery_tags_input/__init__.py
@@ -4,5 +4,5 @@ __author__ = 'Rick van Hattem'
 __author_email__ = 'Rick.van.Hattem@Fawo.nl'
 __description__ = ('''Django jQuery Tags Input is a Django field and widget'''
     '''wrapper that uses `django-selectable for autocompletion''')
-__url__ ='https://github.com/WoLpH/django-jquery-tags-input'
+__url__ = 'https://github.com/WoLpH/django-jquery-tags-input'
 

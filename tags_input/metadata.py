7fca78683898464ac26aac3d5808d8fc57e89898
WoLpH <Wolph@wol.ph>
2014-02-12 17:12:40 +0100

    split media requirements for normal and admin widget

--- tags_input/metadata.py
+++ tags_input/metadata.py
@@ -1,5 +1,5 @@
 __package_name__ = 'django-tags-input'
-__version__ = '1.8.4'
+__version__ = '1.8.5'
 __author__ = 'Rick van Hattem'
 __author_email__ = 'Rick.van.Hattem@Fawo.nl'
 __description__ = '''Django jQuery Tags Input is a Django field and widget

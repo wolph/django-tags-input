8352b10b17304ba5095e0dd2f269b5cc1c6e3b19
Rick van Hattem <Wolph@wol.ph>
2019-05-15 18:38:39 +0200

    made tests compatible with newer django releases

--- example/db_router.py
+++ example/db_router.py
@@ -1,4 +1,3 @@
-
 class Router(object):
     TABLE_MAPPINGS = {
         'autocompletionexample_extraspam': 'other',
@@ -11,16 +10,17 @@ class Router(object):
     db_for_read = db_for_write = get_db
 
     def allow_migrate(self, *args, **hints):
-        # Django 1.8, 1.9 and 1.10 all have different behaviour... sigh
+        # Django 1.8, 1.9, 1.10 and 2.2 all have different behaviour... sigh
         if 'model_name' in hints:
             model_name = hints['model_name']
         elif 'model' in hints:
             model_name = hints['model']._meta.db_table
-        else:
+        elif hasattr(args[1], '_meta'):
             model_name = args[1]._meta.db_table
+        else:
+            model_name = args[1]
 
         return self.TABLE_MAPPINGS.get(model_name, 'default')
 
     def allow_relation(self, obj1, obj2, **hints):
         return True
-

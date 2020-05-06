144fb07eb7d0201e5fddcec480fdd0b67bb2c1b8
Rick van Hattem <Wolph@wol.ph>
2020-05-06 23:34:21 +0200

    Incrementing version to v5.0.0

--- tags_input/__about__.py
+++ tags_input/__about__.py
@@ -1,5 +1,5 @@
 __package_name__ = 'django-tags-input'
-__version__ = '4.6.0'
+__version__ = '5.0.0'
 __author__ = 'Rick van Hattem'
 __author_email__ = 'wolph@wol.ph'
 __description__ = ' '.join('''
